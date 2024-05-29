1b405def1687fa02eabb1aedf2e106165a234d7d
Sachin <raisethebat2020@gmail.com>
2024-05-30 00:53:42 +0530

    initial neccessary configurations

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const {Chess} = require('chess.js');
+const path = require('path');
+const { title } = require('process');
+
+const app = express();
+const server = http.createServer(app);
+
+const io=socket(server);
+
+const chess =new Chess();
+let players = {};
+let currentPlayer = "W";
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    res.render("index", { title : "Chess Game" } );
+});
+
+io.on("connection",function(socket){
+    console.log("connected");
+});
+
+server.listen(5173,function(){
+    console.log("all ok");
+});
